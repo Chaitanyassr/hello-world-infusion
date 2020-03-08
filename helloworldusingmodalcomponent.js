f256e12c5f0204866cf78b908b01a7599bcf9885
CHAITANYA <Chaitanyas0101@gmail.com>
2020-03-08 10:45:33 +0530

    Create helloworldusingmodalcomponent.js

---
+++ helloworldusingmodalcomponent.js
@@ -0,0 +1,31 @@
+fluid.defaults("fluidTutorial.helloWorld", {
+    gradeNames: ["fluid.modelComponent"],
+    model: {
+        message: "Hello, World!"
+    },
+    modelListeners: {
+        // On the left side, configures a
+        // model listener listening to the
+        // 'message' value
+        //
+        // On the right side, configures the
+        // listener to call the component's
+        // 'sayHello' invoker whenever the
+        // value changes
+        message: "{that}.sayHello"
+    },
+    invokers: {
+        sayHello: {
+            funcName: "fluidTutorial.helloWorld.consoleHello",
+            args: ["{that}.model.message"]
+        }
+    }
+});
+
+fluidTutorial.helloWorld.consoleHello = function (message) {
+    console.log(message);
+};
+
+$(document).ready(function () {
+  helloWorld = fluidTutorial.helloWorld();
+});
